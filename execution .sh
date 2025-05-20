a1a176993582186affd52d080ac0ad7be0663c38
h3joseph <josephhaccandy@gmail.com>
2025-05-20 02:09:13 +0200

    Initial commit with index.html and styles.css

---
+++ execution .sh
@@ -0,0 +1,8 @@
+cd /home/joseph/gp-emmanuel-nantes/gp-emmanuel-nantes
+rm -rf .git
+git init
+git add .
+git commit -m "Initial commit with index.html and styles.css"
+git remote add origin https://github.com/h3joseph/groupe-de-priere.git
+git push -u origin master
+git branch
